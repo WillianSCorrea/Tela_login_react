cfe0afb55a719bb4a4086b9b37be77d0fd29e187
Willian Da Silva Correa <willianwcorrea@gmail.com>
2025-02-24 00:37:35 -0300

    Fim da Estrutura Html do jsx do Componente Login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,18 +1,48 @@
-import {FaUser , FaLock} from "react-icons/fa"
+import {FaUser , FaLock} from "react-icons/fa";
+
+import {useState} from 'react';
+
+import "./Login.css";
 const Login = () => {
+    
+    const [username, setUsername] = useState("");
+    const [userpassword, setPassword] = useState("");
+   
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Envio")
+
+    };
+
   return (
     <div className="container">
-      <form action="">
+      <form  onSubmit={handleSubmit}>
         <h1>Acesse o Sistema</h1>
         <div>
-            <input type="email" name="" id="" placeholder="E-mail" />
+            <input type="email" name="" id="" placeholder="E-mail" value={username}
+  onChange={(e) => setUsername(e.target.value)}/>
             <FaUser/>
         </div>
         <div>
-            <input type="password" name="" id="" placeholder="Senha"/>
+            <input type="password" name="" id="" placeholder="Senha" value={userpassword}
+  onChange={(e) => setPassword(e.target.value)}/>
             <FaLock/>
         </div>
+
+        <div className="recall-forget">
+            <label>
+                <input type="checkbox" name="" id="" />Lembre de Mim
+            </label>
+            <a href="">Esqueceu a senha ?</a>
+        </div>
+
+
         <button>Entrar</button>
+
+        <div className="signup-link">
+            <p>Não tem conta? <a href=""> Registrar</a></p>
+        </div>
+
       </form>
     </div>
   )
